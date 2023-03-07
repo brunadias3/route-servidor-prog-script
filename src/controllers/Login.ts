d72991ccbef422604a4095f1897ecdb247d19160
Bruna <brunadias_30@hotmail.com>
2023-03-07 11:07:31 -0300

    gerando tokens de login

---
+++ src/controllers/Login.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import { generateToken } from '../middlewares'
+
+class Login {
+    public async login(req:Request, res:Response) {
+        let {mail} = req.body
+        const r = await generateToken({mail});
+        return res.json({r})
+    }
+}
+
+export default new Login();
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import data from './data'
 import { Router } from 'express'
 import math from './matematica'
+import Login from '../controllers/Login';
 
 const routes = Router();
 
 routes.use("/date", data)
 routes.use("/math", math)
-export default routes
+routes.post("/login", Login.login);
+
+export default routes;
