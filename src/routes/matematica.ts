a8577058e4d40166a5346d04814bba1aa926a4b1
Bruna <brunadias_30@hotmail.com>
2023-03-13 08:22:55 -0300

    adicionando autenticação de admin no sub

--- src/routes/matematica.ts
+++ src/routes/matematica.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import Matematica from "../controllers/Matematica"
+import { authAdmin } from "../middlewares";
 
 const routes = Router();
 
 routes.get("/add", Matematica.somar)
-routes.get("/sub", Matematica.subtrair)
+routes.get("/sub", authAdmin, Matematica.subtrair)
 
 export default routes
