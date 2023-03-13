d620b63ba39d790b553a138a66d7924c5254b545
Bruna <brunadias_30@hotmail.com>
2023-03-13 07:19:21 -0300

    adicionando perfil

--- src/controllers/Login.ts
+++ src/controllers/Login.ts
@@ -1,12 +1,12 @@
-import { Request, Response } from "express";
-import { generateToken } from '../middlewares'
+import { Request, Response } from 'express';
+import { generateToken } from '../middlewares';
 
 class Login {
-    public async login(req:Request, res:Response) {
-        let {mail} = req.body
-        const r = await generateToken({mail});
-        return res.json({r})
+    public async logar(req: Request, res: Response): Promise<Response> {
+        const { mail, profile } = req.body;
+        const token = await generateToken({ mail, profile });
+        return res.json({ token });
     }
 }
 
-export default new Login();
+export default new Login(); 
