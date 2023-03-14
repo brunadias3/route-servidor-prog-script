b63f938f2612b8b3213ef8129cb90a3b5ccff25b
Bruna <brunadias_30@hotmail.com>
2023-03-14 10:20:55 -0300

    criando migration e alterando versao do typeorm

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,25 +1,21 @@
-import { DataSource } from "typeorm"
-import * as dotenv from 'dotenv'
-dotenv.config()
+import { DataSource } from "typeorm";
 
 //https://orkhan.gitbook.io/typeorm/docs/data-source-options
-export const AppDataSource = new DataSource({
-    //url: process.env.BD_URL,
-    host:'localhost',
-    port:5432,
-    username:'postgres',
-    password:'123',
-    database:'bdaula',
-    type: "postgres",
-    synchronize: false, // true indica que o schema do BD será criado a cada vez que a aplicação inicializar
-    logging: false, // true indica que as consultas e erros serão exibidas no terminal
-    entities: ["src/entity/*.ts"], // entidades que serão convertidas em tabelas
+
+//versão para SQLite
+const AppDataSource = new DataSource({
+    database: 'bdaula.sqlite',
+    type: "sqlite", // se for SQLite, então use sqlite
+    // true indica que o schema do BD será criado a cada vez que a aplicação inicializar
+    // deixe false ao usar migrations
+    synchronize: false, 
+    logging: true, // true indica que as consultas e erros serão exibidas no terminal
+    entities: ["src/entities/*.ts"], // entidades que serão convertidas em tabelas
     migrations: ["src/migrations/*.ts"], // local onde estarão os arquivos de migração
     subscribers: [],
     maxQueryExecutionTime: 2000 // 2 seg.
-})
+});
 
-// https://orkhan.gitbook.io/typeorm/docs/data-source
 AppDataSource
     .initialize()
     .then(() => {
@@ -27,4 +23,37 @@ AppDataSource
     })
     .catch((e) => {
         console.error("Erro na inicialização do Data Source:", e)
-    })
+    });
+
+export default AppDataSource;
+
+// import { DataSource } from "typeorm"
+// import * as dotenv from 'dotenv'
+// dotenv.config()
+
+// //https://orkhan.gitbook.io/typeorm/docs/data-source-options
+// export const AppDataSource = new DataSource({
+//     //url: process.env.BD_URL,
+//     host:'localhost',
+//     port:5432,
+//     username:'postgres',
+//     password:'123',
+//     database:'bdaula',
+//     type: "postgres",
+//     synchronize: false, // true indica que o schema do BD será criado a cada vez que a aplicação inicializar
+//     logging: false, // true indica que as consultas e erros serão exibidas no terminal
+//     entities: ["src/entity/*.ts"], // entidades que serão convertidas em tabelas
+//     migrations: ["src/migrations/*.ts"], // local onde estarão os arquivos de migração
+//     subscribers: [],
+//     maxQueryExecutionTime: 2000 // 2 seg.
+// })
+
+// // https://orkhan.gitbook.io/typeorm/docs/data-source
+// AppDataSource
+//     .initialize()
+//     .then(() => {
+//         console.log("Data Source inicializado!")
+//     })
+//     .catch((e) => {
+//         console.error("Erro na inicialização do Data Source:", e)
+//     })
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,13 +1,30 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import * as bcrypt from "bcrypt"
 
 @Entity({name:"users"})
 export class User {
+    //define a chave primária como auto incremento
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({length:50,nullable:false,unique:true})
+    @Column({ nullable:false,unique:true,length:70})
     mail: string;
 
-    @Column({length:100,nullable:false})
+    @Column({ nullable:false, select:false, length:100})
     password: string;
+
+    @BeforeInsert() // A função hashPassword é disparada antes do insert e update
+    @BeforeUpdate()
+    hashPassword(): void {
+        if (this.password) {
+            // A senha é codificada usando o algoritmo do pacote bcrypt
+            this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(10));
+        }
+    }
+
+    compare(input:string): Promise<boolean> {
+        // A senha fornecida em input é comparada com a senha do registro armazenado no SGBD
+        return bcrypt.compare(input, this.password)
+    }
+
 }
